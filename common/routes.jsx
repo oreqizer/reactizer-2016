5eb4068a82543b797595b6ba5a2aa794440c4498
oreqizer <oreqizer@gmail.com>
2016-04-24 21:06:57 +0200

    dev: screw git's renaming

--- common/routes.jsx
+++ common/routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router';
 
-import Index from './paths/index';
+import Index from './paths/Inndex';
 
 export default (
     <Route name="app" component={Index} path="/" />
