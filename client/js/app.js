44c4df914247c60bf5558f47cedc3ebb06a66b90
oreqizer <oreqizer@gmail.com>
2016-04-28 16:01:25 +0200

    new: working on hmr

--- client/js/app.js
+++ client/js/app.js
@@ -17,11 +17,8 @@ render(
 /* eslint-disable */
 if (module.hot) {
   module.hot.accept('./modules/Root.jsx', () => {
-    // TODO: make babel preset 'es2015-webpack' work
-    // to allow a simple usage of <Root /> instead of re-importing
-    const NewRoot = require('./modules/Root.jsx');
     render(
-      <NewRoot />,
+      <Root />,
       document.getElementById('react-view')
     );
   })
