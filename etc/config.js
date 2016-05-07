da93a63ac7307299c59f8c8ba7fa94d072ed4e5f
oreqizer <oreqizer@gmail.com>
2016-05-07 21:48:09 +0200

    config stuff

--- etc/config.js
+++ etc/config.js
@@ -1,10 +1,17 @@
 import nconf from 'nconf';
 
+const DEV = process.env.NODE_ENV === 'dev';
 const PRODUCTION = process.env.NODE_ENV === 'production';
 
+// "foo__bar=lol <command>" becomes "{foo: {bar: 'lol'}}" here
 nconf.env('__');
 
+// DEV: secrets in some JSON file, TODO: add one
+// PRODUCTION: secrets from environment variables
+// see: https://github.com/este/este/blob/master/src/server/config.js
+
 nconf.defaults({
+  DEV,
   PRODUCTION,
   DEFAULT_LOCALE: 'en',
   GOOGLE_ANALYTICS_ID: 'UA-XXXXXXX-X',
