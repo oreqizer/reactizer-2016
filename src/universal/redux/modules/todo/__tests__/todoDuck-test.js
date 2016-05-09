d60fd70c70e7a6126b78a45fc3b4f3afe40a2e92
oreqizer <oreqizer@gmail.com>
2016-05-09 22:43:40 +0200

    finished todo duck tests

--- src/universal/redux/modules/todo/__tests__/todoDuck-test.js
+++ src/universal/redux/modules/todo/__tests__/todoDuck-test.js
@@ -1,17 +1,19 @@
 import { List } from 'immutable';
 
-import { CLEAN } from '../../../../consts/stateConsts';
+import { CLEAN, SUCCESS, LOADING } from '../../../../consts/stateConsts';
 
 import reducer, * as actions from '../todoDuck';
 import {
   FETCH,
+  FETCH_SUCCESS,
   CREATE,
+  CREATE_SUCCESS,
   EDIT,
   DELETE,
 } from '../todoActions';
 
-jest.unmock('../todoDuck');
 jest.unmock('immutable');
+jest.unmock('../todoDuck');
 
 describe('todo action creators', () => {
   it('should make a fetch action', () => {
@@ -58,6 +60,81 @@ describe('todo action creators', () => {
 describe('todo reducer', () => {
   it('should return initial state', () => {
     const { list, state } = reducer(undefined, {});
+
+    expect(list.equals(new List())).toBe(true);
+    expect(state).toBe(CLEAN);
+  });
+
+  it('should handle FETCH request', () => {
+    const { list, state } = reducer(undefined, {
+      type: FETCH,
+    });
+
+    expect(list.equals(new List())).toBe(true);
+    expect(state).toBe(LOADING);
+  });
+
+  it('should handle CREATE request', () => {
+    const { list, state } = reducer(undefined, {
+      type: CREATE,
+      text: 'testing',
+    });
+
+    expect(list.equals(new List())).toBe(true);
+    expect(state).toBe(LOADING);
+  });
+
+  it('should handle FETCH_SUCCESS', () => {
+    const todos = ['a todo', 'two todos'];
+    const { list, state } = reducer(undefined, {
+      type: FETCH_SUCCESS,
+      todos,
+    });
+
+    expect(list.equals(new List(todos))).toBe(true);
+    expect(state).toBe(SUCCESS);
+  });
+
+  it('should handle CREATE_SUCCESS', () => {
+    const text = 'testing';
+    const { list, state } = reducer(undefined, {
+      type: CREATE_SUCCESS,
+      text,
+    });
+
+    expect(list.equals(new List([text]))).toBe(true);
+    expect(state).toBe(SUCCESS);
+  });
+
+  it('should handle EDIT', () => {
+    const old = 'testing123';
+    const text = 'testing456';
+
+    const initialized = reducer({
+      list: [old],
+    });
+
+    const { list, state } = reducer(initialized, {
+      type: EDIT,
+      id: 0,
+      text,
+    });
+
+    expect(list.equals(new List([text]))).toBe(true);
+    expect(state).toBe(CLEAN);
+  });
+
+  it('should handle DELETE', () => {
+    const old = 'testing123';
+
+    const initialized = reducer({
+      list: [old],
+    });
+
+    const { list, state } = reducer(initialized, {
+      type: DELETE,
+      id: 0,
+    });
 
     expect(list.equals(new List())).toBe(true);
     expect(state).toBe(CLEAN);
