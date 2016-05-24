926827be41c653e97a8d4d6c93c28ee54c0a059a
oreqizer <oreqizer@gmail.com>
2016-05-24 23:35:34 +0200

    composing form, added eslint to web

---
+++ src/web/modules/Signup/SignupForm.jsx
@@ -0,0 +1,6 @@
+import React, { PropTypes, Component } from 'react';
+import { injectIntl, defineMessages, FormattedMessage } from 'react-intl';
+
+export default class SignupForm extends Component {
+  
+}
--- src/web/modules/Signup/index.jsx
+++ src/web/modules/Signup/index.jsx
@@ -1,11 +1,8 @@
-import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
-import { reduxForm, Field } from 'redux-form';
-import { Paper, TextField } from 'material-ui';
+import { Paper } from 'material-ui';
 
-import { login, register } from '../../../universal/redux/modules/user/userDuck';
-import { userMessages, formMessages } from '../../../universal/messages';
+import SignupForm from './SignupForm';
 
 const messages = defineMessages({
   signup: {
@@ -14,34 +11,16 @@ const messages = defineMessages({
   },
 });
 
-@reduxForm({
-  form: 'signup',
-})
-@connect(state => ({
-  user: state.user,
-}))
-export default class Signup extends Component {
-  render() {
-    return (
-      <div id="Login">
-        <div className="markdown-body">
-          <h2>
-            <FormattedMessage {...messages.signup} />
-          </h2>
-        </div>
-        <Paper>
-          <Field
-            name="username"
-            component={username =>
-              <TextField
-                hintText={<FormattedMessage {...formMessages.enter} field="username" />}
-                floatingLabelText={"username"}
-                {...username}
-              />
-            }
-          />
-        </Paper>
-      </div>
-    );
-  }
-}
+const Signup = props =>
+  <div id="Login">
+    <div className="markdown-body">
+      <h2>
+        <FormattedMessage {...messages.signup} />
+      </h2>
+    </div>
+    <Paper>
+      <SignupForm />
+    </Paper>
+  </div>;
+
+export default Signup;
