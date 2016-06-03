3d7288618faaeec85ca692dd71505fff3c12740a
oreqizer <oreqizer@gmail.com>
2016-06-03 18:03:36 +0200

    sorting data

--- src/universal/modules/todo/todoMapper.js
+++ src/universal/modules/todo/todoMapper.js
@@ -1,7 +1,10 @@
-import { OrderedMap } from 'immutable';
+import { Map } from 'immutable';
+import { sort } from 'ramda';
 
 import Todo from '../../containers/Todo';
 
 export function toMap(todos = []) {
-  return todos.reduce((map, todo) => map.set(todo.id, new Todo(todo)), new Map());
+  // data need to be sorted for checksums to match on client/server
+  return sort((t1, t2) => t1.id - t2.id, todos)
+    .reduce((map, todo) => map.set(todo.id, new Todo(todo)), new Map());
 }
