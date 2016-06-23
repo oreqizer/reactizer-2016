76a5d830412be7724a78f38a061b512599464cec
oreqizer <oreqizer@gmail.com>
2016-06-23 22:15:41 +0200

    Index simplify and cleanup

--- src/browser/js/modules/Index.jsx
+++ src/browser/js/modules/Index.jsx
@@ -1,10 +1,10 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
+import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
 import { defineMessages, injectIntl, intlShape } from 'react-intl';
 import { AppBar, FontIcon } from 'material-ui';
 import Helmet from 'react-helmet';
-import { autobind } from 'core-decorators';
 
 import intlProvider from '../../../universal/decorators/intlProvider';
 import { toggleSidebar } from '../../../universal/modules/ui/uiDuck';
@@ -23,67 +23,46 @@ const messages = defineMessages({
   },
 });
 
-@intlProvider
-@injectIntl
-@connect(state => ({
-  appName: state.config.appName,
-}), { push, toggleSidebar })
-export default class Index extends Component {
-  static propTypes = {
-    intl: intlShape.isRequired,
-    appName: PropTypes.string.isRequired,
-    push: PropTypes.func.isRequired,
-    toggleSidebar: PropTypes.func.isRequired,
-    children: PropTypes.node.isRequired,
-  };
+const Index = props =>
+  <div id="Index">
+    <Helmet
+      title={props.intl.formatMessage(messages.title)}
+      titleTemplate={`%s | ${props.appName}`}
+      meta={[
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: props.intl.formatMessage(messages.description) },
+        { property: 'og:type', content: 'boilerplate' },
+      ]}
+    />
+    <AppBar
+      title={<span style={{ cursor: 'pointer' }}>{props.appName}</span>}
+      iconElementRight={
+        <a href="https://github.com/oreqizer/reactizer" target="_blank" rel="noopener noreferrer">
+          <FontIcon className="octocat" style={{ margin: 12 }} />
+        </a>
+      }
+      onTitleTouchTap={() => props.push('/')}
+      onLeftIconButtonTouchTap={props.toggleSidebar}
+    />
+    <Sidebar />
+    <div id="container">
+      {props.children}
+    </div>
+  </div>;
 
-  constructor(props) {
-    super(props);
+Index.propTypes = {
+  intl: intlShape.isRequired,
+  appName: PropTypes.string.isRequired,
+  push: PropTypes.func.isRequired,
+  toggleSidebar: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+};
 
-    this.state = {
-      open: false,
-    };
-  }
-
-  @autobind
-  handleToggleDrawer() {
-    this.props.toggleSidebar();
-  }
-
-  renderGithub() {
-    return (
-      <a href="https://github.com/oreqizer/reactizer" target="_blank" rel="noopener noreferrer">
-        <FontIcon className="octocat" style={{ margin: 12 }} />
-      </a>
-    );
-  }
-
-  render() {
-    const { intl, children, appName } = this.props;
-
-    return (
-      <div id="Index">
-        <Helmet
-          title={intl.formatMessage(messages.title)}
-          titleTemplate={`%s | ${appName}`}
-          meta={[
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { name: 'description', content: intl.formatMessage(messages.description) },
-            { property: 'og:type', content: 'boilerplate' },
-          ]}
-        />
-        <AppBar
-          title={<span style={{ cursor: 'pointer' }}>{appName}</span>}
-          iconElementRight={this.renderGithub()}
-          onTitleTouchTap={() => this.props.push('/')}
-          onLeftIconButtonTouchTap={this.handleToggleDrawer}
-        />
-        <Sidebar />
-        <div id="container">
-          {children}
-        </div>
-      </div>
-    );
-  }
-}
+export default compose(
+  intlProvider,
+  injectIntl,
+  connect(state => ({
+    appName: state.config.appName,
+  }), { push, toggleSidebar })
+)(Index);
